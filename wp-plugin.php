a24f14594be8121ad49dffba62d8346af85ecf4e
Dang Vu <minhtri.contact@gmail.com>
2017-08-16 10:52:29 +0700

    move hooks into constructor

--- wp-plugin.php
+++ wp-plugin.php
@@ -5,7 +5,6 @@
  * Description: My simple WordPress plugin boilerplate.
  * Author:      i30
  * Version:     1.0.0
- * Author URI:  https://i30.github.com/
  * Text Domain: wp-plugin
  */
 final class WPPlugin
@@ -39,6 +38,10 @@ final class WPPlugin
         $this->settings = $settings ? (array)$settings : array();
         $this->settings['basedir']  = __DIR__ . '/';
         $this->settings['baseuri']  = plugins_url( '/', __FILE__ );
+
+        add_action('activate_wp-plugin/wp-plugin.php', array($this, '_activate'));
+        add_action('deactivate_wp-plugin/wp-plugin.php', array($this, '_deactivate'));
+        add_action('plugins_loaded', array($this, '_install'), 10, 0);
     }
 
     /**
@@ -50,7 +53,7 @@ final class WPPlugin
      *
      * @param    bool    $network    Whether activating this plugin on network or a single site.
      */
-    function activate($network)
+    function _activate($network)
     {
         add_option(self::OPTION_NAME, array(
 
@@ -64,7 +67,7 @@ final class WPPlugin
      *
      * @see    https://developer.wordpress.org/reference/hooks/plugins_loaded/
      */
-    function install()
+    function _install()
     {
         load_plugin_textdomain('wp-plugin', false, $this->settings['basedir'] . 'languages');
 
@@ -79,38 +82,11 @@ final class WPPlugin
      *
      * @param    bool    $network    Whether deactivating this plugin on network or a single site.
      */
-    function deactivate($network)
-    {
-
-    }
-
-    /**
-     * Do uninstallation
-     *
-     * @internal    Used as a callback. PLEASE DO NOT RECALL THIS METHOD DIRECTLY!
-     *
-     * @see    https://developer.wordpress.org/reference/functions/register_uninstall_hook/
-     */
-    static function uninstall()
+    function _deactivate($network)
     {
 
     }
 }
 
-// Get CleverMegaMenu instance.
-$plugin = new WPPlugin(get_option(WPPlugin::OPTION_NAME));
-
-// Register activation hook.
-register_activation_hook(__FILE__, array($plugin, 'activate'));
-
-// Register deactivation hook.
-register_deactivation_hook(__FILE__, array($plugin, 'deactivate'));
-
-// Register installation hook.
-add_action('plugins_loaded', array($plugin, 'install'), 10, 0);
-
-// Register uninstallation hook.
-register_uninstall_hook(__FILE__, 'WPPlugin::uninstall');
-
-// Unset plugin instance from global space.
-unset($plugin);
+// Initialize plugin.
+return new WPPlugin(get_option(WPPlugin::OPTION_NAME));
