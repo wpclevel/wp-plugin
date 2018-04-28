05a2fa30ad66dd4e494045a0a453a8782766d2c3
Minh Tri <minhtri.contact@gmail.com>
2018-04-28 10:43:23 +0700

    initial commit

---
+++ src/Helpers/Autoloader.php
@@ -0,0 +1,82 @@
+<?php namespace WP\Plugins\SamplePlugin\Helpers;
+
+use InvalidArgumentException;
+
+/**
+ * Autoloader
+ *
+ * !WARNING: Only works with PSR-4 and unqualified classes.
+ */
+final class Autoloader
+{
+    /**
+     * Namespace
+     *
+     * @var  string
+     */
+    private $ns;
+
+    /**
+     * Base directory
+     *
+     * @var  string
+     */
+    private $dir;
+
+    /**
+     * Constructor
+     */
+    private function __construct()
+    {
+        // No reconstruction.
+    }
+
+    /**
+     * Singleton
+     */
+    static function init()
+    {
+        static $self = null;
+
+        if (null === $self) {
+            $self = new self;
+        }
+
+        return $self;
+    }
+
+    /**
+     * Autoload PSR-4 classes inside a directory.
+     *
+     * @param  string  $ns   Namespace prefix.
+     * @param  string  $dir  Base directory.
+     */
+    function load($ns, $dir)
+    {
+        $this->ns = trim($ns, '\\');
+        $this->dir = rtrim($dir, '/');
+
+        spl_autoload_register([$this, 'loadClass'], true, false);
+    }
+
+    /**
+     * Load file for the given class name
+     *
+     * @param  string  $class
+     */
+    private function loadClass($class)
+    {
+        if (0 !== strpos($class, $this->ns)) {
+            return;
+        }
+
+        $path = str_replace($this->ns, $this->dir, $class);
+        $file = str_replace('\\', '/', $path) . '.php';
+
+        if (file_exists($file)) {
+            require $file;
+        } else {
+            throw new InvalidArgumentException(sprintf(__('Class %s not found.', 'wp-plugin'), $class));
+        }
+    }
+}
