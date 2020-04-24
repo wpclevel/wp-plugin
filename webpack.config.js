d257e4207c3257bcf2e4212bc5e2e805324a5b33
Minh Tri <minhtri.contact@gmail.com>
2020-04-25 01:35:14 +0700

    refer common ex WordPress components

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,20 @@ module.exports = {
             loader: 'babel-loader'
         }]
     },
+    externals: {
+      '@wordpress/i18n': 'wp.i18n',
+      '@wordpress/hooks': 'wp.hooks',
+      '@wordpress/blocks': 'wp.blocks',
+      '@wordpress/editor': 'wp.editor',
+      '@wordpress/plugins': 'wp.plugins',
+      '@wordpress/compose': 'wp.compose',
+      '@wordpress/element': 'wp.element',
+      '@wordpress/api-fetch': 'wp.apiFetch',
+      '@wordpress/edit-post': 'wp.editPost',
+      '@wordpress/components': 'wp.components',
+      '@wordpress/block-editor': 'wp.blockEditor',
+      '@wordpress/html-entities': 'wp.htmlEntities'
+    },
     watch: true,
     watchOptions: {
         ignored: ['**/*.min.js']
