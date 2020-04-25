76fbe4ba1e935edba369bb7687a8de1da1b3d9bf
Minh Tri <minhtri.contact@gmail.com>
2020-04-25 17:22:03 +0700

    add more ex components

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,40 @@
-const path = require('path'),
-    contextPath = path.resolve(__dirname, 'assets/js');
+const path = require('path');
 
+// Sample configs.
 module.exports = {
-    mode: 'development',
-    context: contextPath,
-    entry: {
-        
-    },
-    output: {
-        path: contextPath,
-        filename: '[name].min.js'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-        }]
-    },
-    externals: {
-      '@wordpress/i18n': 'wp.i18n',
-      '@wordpress/hooks': 'wp.hooks',
-      '@wordpress/blocks': 'wp.blocks',
-      '@wordpress/editor': 'wp.editor',
-      '@wordpress/plugins': 'wp.plugins',
-      '@wordpress/compose': 'wp.compose',
-      '@wordpress/element': 'wp.element',
-      '@wordpress/api-fetch': 'wp.apiFetch',
-      '@wordpress/edit-post': 'wp.editPost',
-      '@wordpress/components': 'wp.components',
-      '@wordpress/block-editor': 'wp.blockEditor',
-      '@wordpress/html-entities': 'wp.htmlEntities'
-    },
-    watch: true,
-    watchOptions: {
-        ignored: ['**/*.min.js']
-    }
+  mode: 'development',
+  entry: {
+    // Enter your entries here {name:file}.
+  },
+  output: {
+    path: path.resolve(__dirname, 'assets/js'),
+    filename: '[name].min.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+  externals: {
+    'lodash': 'lodash',
+    '@wordpress/data': 'wp.data',
+    '@wordpress/i18n': 'wp.i18n',
+    '@wordpress/hooks': 'wp.hooks',
+    '@wordpress/blocks': 'wp.blocks',
+    '@wordpress/editor': 'wp.editor',
+    '@wordpress/plugins': 'wp.plugins',
+    '@wordpress/compose': 'wp.compose',
+    '@wordpress/element': 'wp.element',
+    '@wordpress/api-fetch': 'wp.apiFetch',
+    '@wordpress/edit-post': 'wp.editPost',
+    '@wordpress/components': 'wp.components',
+    '@wordpress/block-editor': 'wp.blockEditor',
+    '@wordpress/html-entities': 'wp.htmlEntities'
+  },
+  watch: true,
+  watchOptions: {
+    ignored: ['**/*.min.js']
+  }
 }
